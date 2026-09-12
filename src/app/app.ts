12f9624b7841e211ed167b3135aab62024c0c873
JorgeTech26 <jorge.luis.tech26@gmail.com>
2026-09-12 08:36:52 -0500

    feat: agregar estructura principal de la aplicación

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './shared/components/footer/footer';
+import { Navbar } from './shared/components/navbar/navbar';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Navbar],
   selector: 'app-root',
   templateUrl: './app.html',
 })
