08b43c2c0d2f2572253211d3169e027444d30bd0
JorgeTech26 <jorge.luis.tech26@gmail.com>
2026-09-13 09:28:13 -0500

    feat: agregar rutas de navegación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './gifs/pages/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'gifs',
+    component: HomePage,
+  },
+  {
+    path: 'buscar-gif',
+    loadComponent: () => import('./gifs/pages/search-page/search-page').then((m) => m.SearchPage),
+  },
+  {
+    path: '**',
+    redirectTo: 'gifs',
+  },
+];
