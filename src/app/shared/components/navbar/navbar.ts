f529dde6eed215552ec1c33d07bd6c591d8525b1
JorgeTech26 <jorge.luis.tech26@gmail.com>
2026-09-14 01:07:50 -0500

    refactor: extraer navbar brand, navbar item y menú hamburguesa

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NavbarItem } from './navbar-item/navbar-item';
+import { NavbarBrand } from './navbar-brand/navbar-brand';
+import { NavbarHamburger } from './navbar-hamburger/navbar-hamburger';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [NavbarItem, NavbarBrand, NavbarHamburger],
   templateUrl: './navbar.html',
 })
 export class Navbar {
