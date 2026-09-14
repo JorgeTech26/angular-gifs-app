ba53f2e32f5b1af0ce10a8530f445b70a9f4fa31
JorgeTech26 <jorge.luis.tech26@gmail.com>
2026-09-14 08:45:53 -0500

    feat: agregar componente para la lista de gifs

--- src/app/gifs/pages/home-page/home-page.ts
+++ src/app/gifs/pages/home-page/home-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { GifList } from '../../components/gif-list/gif-list';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [GifList],
   templateUrl: './home-page.html',
 })
 export class HomePage {}
