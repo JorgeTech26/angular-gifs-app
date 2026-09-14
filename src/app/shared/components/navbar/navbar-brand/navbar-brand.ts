bffeb6c3f2623b69c3c728285e9c2f76b9d7a8a0
JorgeTech26 <jorge.luis.tech26@gmail.com>
2026-09-14 01:05:04 -0500

    feat: agregar componente título para reutilizar

---
+++ src/app/shared/components/navbar/navbar-brand/navbar-brand.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar-brand',
+  imports: [RouterLink],
+  templateUrl: './navbar-brand.html',
+})
+export class NavbarBrand {}
